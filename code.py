8505363e113dcd0fc2d93ad39901cf00c2d4bd90
rybnikovanastia <rybnikova.nastia@gmail.com>
2026-04-15 11:23:52 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,80 @@
+import time
+import board
+import analogio
+import usb_hid
+
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.mouse import Mouse
+from adafruit_circuitplayground import cp
+
+# setup
+kbd = Keyboard(usb_hid.devices)
+# mouse = Mouse(usb_hid.devices)
+
+# sensors
+sensorA = analogio.AnalogIn(board.A4)
+sensorB = analogio.AnalogIn(board.A7)
+sensorC = analogio.AnalogIn(board.A0)
+
+# thresholds 
+THRESHOLD_A = 50000
+THRESHOLD_B = 50000
+THRESHOLD_C = 50000
+
+# state tracking
+c_pressed = False
+g_pressed = False
+space_pressed = False
+
+while True:
+    # read sensors
+    valA = sensorA.value
+    valB = sensorB.value
+    valC = sensorC.value
+    print("A:", sensorA.value, "B:", sensorB.value,  "C:", sensorC.value)
+    time.sleep(0.01)
+    # Sensor A
+    if valA > THRESHOLD_A:
+        if not c_pressed:
+            kbd.press(Keycode.C)
+            c_pressed = True
+    else:
+        if c_pressed:
+            kbd.release(Keycode.C)
+            c_pressed = False
+
+    # Sensor B
+    if valB > THRESHOLD_B:
+        if not g_pressed:
+            kbd.press(Keycode.G)
+            g_pressed = True
+    else:
+        if g_pressed:
+            kbd.release(Keycode.G)
+            g_pressed = False
+    # Sensor C
+    if valC > THRESHOLD_C:
+        if not space_pressed:
+            kbd.press(Keycode.SPACE)
+            space_pressed = True
+    else:
+        if space_pressed:
+            kbd.release(Keycode.SPACE)
+            space_pressed = False
+    # # mouse
+    # x, y, z = cp.acceleration
+
+    # move_x = int(x * 2)
+    # move_y = int(y * 2)
+
+    # # stop the mouse
+    # if abs(move_x) < 2:
+    #     move_x = 0
+    # if abs(move_y) < 2:
+    #     move_y = 0
+
+    # mouse.move(move_x, move_y)
+
+    # delay
+    time.sleep(0.01)
